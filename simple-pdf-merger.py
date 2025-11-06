676ccfc971115c509cd319e922d1eee08a876654
Christoffer Braun <chrisbraun99@gmail.com.com>
2025-11-06 00:22:36 -0500

    Added graceful exiting if we close the folder selection dialog box

--- simple-pdf-merger.py
+++ simple-pdf-merger.py
@@ -5,7 +5,10 @@ from PyPDF2 import PdfMerger
 
 root = Tk()
 root.withdraw()
-folder = Path(filedialog.askdirectory(title="Select folder with PDFs"))
+folder = filedialog.askdirectory(title="Select folder with PDFs")
+if not folder:
+    exit("No folder selected")
+folder = Path(folder)
 
 pdfs = sorted([f for f in folder.glob("*.pdf") if "_MERGED" not in f.name], 
               key=lambda x: int(re.search(r'\d+', x.name).group()) if re.search(r'\d+', x.name) else float('inf'))
